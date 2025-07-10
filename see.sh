14416e02fff708f4b549b6ee632b46b74a6a897d
Jubair Ahmad <138237636+jubairbro@users.noreply.github.com>
2025-07-11 00:57:16 +0600

    Update and rename see.txt to see.sh

--- see.sh
+++ see.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#made by Jubair bro 
+# made by Jubair bro
+# হয়তো সব কিছু একদিন সৃতি হয়ে যাবে😔
 set -e
 RESET='\033[0m'; RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[0;33m'; CYAN='\033[0;36m'; BOLD='\033[1m'
 print_header() {
